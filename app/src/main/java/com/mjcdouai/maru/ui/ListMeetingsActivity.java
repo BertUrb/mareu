7136214e59ca53df13eff9bc22fec19393ec3e39
Bertrand URBANIAK <bertrand.urbaniak@mjcdouai.fr>
2021-12-01 17:34:31 +0100

    Add Menu

--- app/src/main/java/com/mjcdouai/maru/ui/ListMeetingsActivity.java
+++ app/src/main/java/com/mjcdouai/maru/ui/ListMeetingsActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 
 import com.mjcdouai.maru.R;
 import com.mjcdouai.maru.databinding.ActivityListMeetingsBinding;
@@ -27,6 +28,10 @@ public class ListMeetingsActivity extends AppCompatActivity {
         setContentView(mBinding.getRoot());
         mBinding.recyclerview.setAdapter(new MeetingRecyclerViewAdapter(mMeetings));
 
-
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
